4d888c698f1da199bf519f01aa25a642dce273e0
toudoryca <tudor.dregan@gmail.com>
2023-07-01 17:48:23 +0300

    swagger documentation

--- src/main/java/com/investment/controller/PriceController.java
+++ src/main/java/com/investment/controller/PriceController.java
@@ -3,8 +3,7 @@ package com.investment.controller;
 import com.investment.dto.CryptoDetails;
 import com.investment.dto.NormalizedCryptoCurrency;
 import com.investment.service.PriceService;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @RestController
@@ -23,16 +24,21 @@ public class PriceController {
         this.priceService = priceService;
     }
 
+    @ApiOperation(value = " Returns a descending of list of all the cryptos " +
+            " sorted descending by normalized range .")
     @GetMapping
     public ResponseEntity<List<NormalizedCryptoCurrency>> getAll() {
         return ResponseEntity.ok(priceService.getAllCrypto());
     }
 
+    @ApiOperation(value = "Returns metrics about a crypto oldest/newest/min/max values")
     @GetMapping("/{symbol}")
     public ResponseEntity<CryptoDetails> getNormalized(@PathVariable("symbol") @NotNull @Valid String symbol) {
         return ResponseEntity.ok(priceService.getDetailedCrypto(symbol));
     }
 
+    @ApiOperation(value = "Return the crypto with the highest normalized range for a\n" +
+            "specific day")
     @GetMapping("/highest")
     public ResponseEntity<NormalizedCryptoCurrency> getHighestNormalizedRangeForDay(@RequestParam String date) {
         return ResponseEntity.ok(priceService.getHighestNormalizedRangeForDay(date));
